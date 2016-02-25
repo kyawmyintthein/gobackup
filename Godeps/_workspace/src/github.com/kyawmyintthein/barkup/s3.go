275ba410c5e0414369a92c7183d8be6de0bfc801
Kyaw Myint Thein <kyawmyintthein2020@gmail.com>
2016-02-25 22:45:26 +0800

    add godep vendoring

---
+++ Godeps/_workspace/src/github.com/kyawmyintthein/barkup/s3.go
@@ -0,0 +1,61 @@
+package barkup
+
+import (
+	"bufio"
+	"gobackup/Godeps/_workspace/src/github.com/goamz/goamz/aws"
+	"gobackup/Godeps/_workspace/src/github.com/goamz/goamz/s3"
+	"os"
+)
+
+// S3 is a `Storer` interface that puts an ExportResult to the specified S3 bucket. Don't use your main AWS keys for this!! Create read-only keys using IAM
+type S3 struct {
+	// Available regions:
+	// * us-east-1
+	// * us-west-1
+	// * us-west-2
+	// * eu-west-1
+	// * ap-southeast-1
+	// * ap-southeast-2
+	// * ap-northeast-1
+	// * sa-east-1
+	Region string
+	// Name of the bucjet
+	Bucket string
+	// AWS S3 access key
+	AccessKey string
+	// AWS S3 secret
+	ClientSecret string
+}
+
+// Store puts an `ExportResult` struct to an S3 bucket within the specified directory
+func (x *S3) Store(result *ExportResult, directory string) *Error {
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	file, err := os.Open(result.Path)
+	if err != nil {
+		return makeErr(err, "")
+	}
+	defer file.Close()
+
+	buffy := bufio.NewReader(file)
+	stat, err := file.Stat()
+	if err != nil {
+		return makeErr(err, "")
+	}
+
+	size := stat.Size()
+
+	auth := aws.Auth{
+		AccessKey: x.AccessKey,
+		SecretKey: x.ClientSecret,
+	}
+
+	s := s3.New(auth, aws.Regions[x.Region])
+	bucket := s.Bucket(x.Bucket)
+	options := s3.Options{}
+	err = bucket.PutReader(directory+result.Filename(), buffy, size, result.MIME, s3.BucketOwnerFull, options)
+	return makeErr(err, "")
+}
--- main.go
+++ main.go
@@ -1,56 +1,55 @@
 package main
 
 import (
-  "os"
-  "log"
-  "path/filepath"
-  "io/ioutil"
-  "github.com/codegangsta/cli"
-  "gopkg.in/yaml.v1"
-  "fmt"
-  "github.com/kyawmyintthein/barkup"
-  "errors"
+	"errors"
+	"fmt"
+	"gobackup/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"gobackup/Godeps/_workspace/src/github.com/kyawmyintthein/barkup"
+	"gobackup/Godeps/_workspace/src/gopkg.in/yaml.v1"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
-
 
 type Adapter int
 
 const (
-     MYSQL        Adapter = 1 + iota
-     POSTGRE       
-     MONGODB       
+	MYSQL Adapter = 1 + iota
+	POSTGRE
+	MONGODB
 )
 
-var adapters = [...]string {
- "mysql",
- "postgre",
- "mongodb",
+var adapters = [...]string{
+	"mysql",
+	"postgre",
+	"mongodb",
 }
 
 type Config struct {
-    Adapter string    `json:"adapter"`
-    Database Database `json:"database"`
-    S3 		 S3       `json:"s3"`
+	Adapter  string   `json:"adapter"`
+	Database Database `json:"database"`
+	S3       S3       `json:"s3"`
 }
 
 type Database struct {
-    Name     	string `default:"test" json:"name"`
-    User     	string `default:"root"`
-    Password 	string `required:"true" env:"DBPassword"`
-    Port     	string   `default:"3306"`
-    Host 		string `default:"localhost"`
+	Name     string `default:"test" json:"name"`
+	User     string `default:"root"`
+	Password string `required:"true" env:"DBPassword"`
+	Port     string `default:"3306"`
+	Host     string `default:"localhost"`
 }
 
 type S3 struct {
-    Bucket     		string `required:"true" json:"name"`
-    Region     		string `required:"true" json:"region"`
-    AccessKey 		string `required:"true" env:"accessKey" json:"access_key"`
-    ClientSecret  	string `required:"true" env:"accessKey" json:"secret"`
-    Path  			string `required:"true"  json:"path"`
+	Bucket       string `required:"true" json:"name"`
+	Region       string `required:"true" json:"region"`
+	AccessKey    string `required:"true" env:"accessKey" json:"access_key"`
+	ClientSecret string `required:"true" env:"accessKey" json:"secret"`
+	Path         string `required:"true"  json:"path"`
 }
 
-
 var config Config
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "gobackup"
@@ -58,77 +57,76 @@ func main() {
 
 	// Commands
 	app.Commands = []cli.Command{
-	    {
-	        Name: "export",
-	        Flags: []cli.Flag{
-	            cli.StringFlag{
-			        Name:  "f, file",
-			        Value: "gobackup.yml",
-			        Usage: "Specify an alternate gobackup config file (default: gobackup.yml)",
-			    },
-			    cli.StringFlag{
-			        Name:  "adapter",
-			        Usage: "Specify an alternate database adapter (default: none)",
-			    },
-	        },
-	        Usage:  "Export database and upload to target source",
-	        Action: func(c *cli.Context) {
-		       log.Println("completed task: ", c.Args().First())
-			   // if len(c.Args()) > 0 {
-			   //  name =  c.Args().First()
-			   // }
-			   file := c.String("file")
-			   if file == ""{
-			   		file = c.String("f")
-			   		if file == ""{
-			   			file ="gobackup.yml"
-			   		}
-			   }
+		{
+			Name: "export",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "f, file",
+					Value: "gobackup.yml",
+					Usage: "Specify an alternate gobackup config file (default: gobackup.yml)",
+				},
+				cli.StringFlag{
+					Name:  "adapter",
+					Usage: "Specify an alternate database adapter (default: none)",
+				},
+			},
+			Usage: "Export database and upload to target source",
+			Action: func(c *cli.Context) {
+				log.Println("completed task: ", c.Args().First())
+				// if len(c.Args()) > 0 {
+				//  name =  c.Args().First()
+				// }
+				file := c.String("file")
+				if file == "" {
+					file = c.String("f")
+					if file == "" {
+						file = "gobackup.yml"
+					}
+				}
 
-			   	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-			    if err != nil {
-			            panic(err)
-			    }
-			    fileName := dir + "/"+ file
-			    yamlFile, err := ioutil.ReadFile(fileName)
-			    if err != nil {
-			        log.Fatal(err)
-			    }
-			    fmt.Println(string(yamlFile))
-				
+				dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+				if err != nil {
+					panic(err)
+				}
+				fileName := dir + "/" + file
+				yamlFile, err := ioutil.ReadFile(fileName)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(yamlFile))
+
 				err = yaml.Unmarshal(yamlFile, &config)
 				fmt.Println(err)
 				if err != nil {
-				    log.Fatal(err)
-				} 
-
+					log.Fatal(err)
+				}
 
 				if err := config.validConfig(); err != nil {
-				    log.Fatal(err)
+					log.Fatal(err)
 				}
 				fmt.Printf("Adapter: %#v\n", config.Adapter)
-			 	switch (config.Adapter){
-				    case MYSQL.String():
-				        _, err := config.exportMysqlDatabase()
-				        fmt.Println(err)
-				        if err == nil{
-				        	log.Fatal(err)
-				        }
-				    case POSTGRE.String():
-				        config.exportMysqlDatabase()
-				    case MONGODB.String():
-				        config.exportMongodbDatabase()
-			    }
-    			
-		    },
-	    },
+				switch config.Adapter {
+				case MYSQL.String():
+					_, err := config.exportMysqlDatabase()
+					fmt.Println(err)
+					if err == nil {
+						log.Fatal(err)
+					}
+				case POSTGRE.String():
+					config.exportMysqlDatabase()
+				case MONGODB.String():
+					config.exportMongodbDatabase()
+				}
+
+			},
+		},
 	}
 	app.Run(os.Args)
 }
 
-func (config *Config) validConfig() error{
+func (config *Config) validConfig() error {
 	if config.Adapter == "" {
-		return  errors.New("Adapter not found in config file.")
+		return errors.New("Adapter not found in config file.")
 	}
 
 	// if config.Database{
@@ -142,43 +140,43 @@ func (config *Config) validConfig() error{
 	return nil
 }
 
-func (config *Config) exportMysqlDatabase() (bool,error){
-	 // Configure a MySQL exporter
-    mysql := &barkup.MySQL{
-        Host:     config.Database.Host,
-        Port:     config.Database.Port,
-        DB:       config.Database.Name,
-        User:     config.Database.User,
-        Password: config.Database.Password,
-    }
+func (config *Config) exportMysqlDatabase() (bool, error) {
+	// Configure a MySQL exporter
+	mysql := &barkup.MySQL{
+		Host:     config.Database.Host,
+		Port:     config.Database.Port,
+		DB:       config.Database.Name,
+		User:     config.Database.User,
+		Password: config.Database.Password,
+	}
 
-    // Configure a S3 storer
-    s3 := &barkup.S3{
-        Region:       config.S3.Region,
-        Bucket:       config.S3.Bucket,
-        AccessKey:    config.S3.AccessKey,
-        ClientSecret: config.S3.ClientSecret,
-    }
+	// Configure a S3 storer
+	s3 := &barkup.S3{
+		Region:       config.S3.Region,
+		Bucket:       config.S3.Bucket,
+		AccessKey:    config.S3.AccessKey,
+		ClientSecret: config.S3.ClientSecret,
+	}
 
-    // Export the database, and send it to the
-    // bucket in the `db_backups` folder
-    err := mysql.Export().To(config.S3.Path, s3)
-    if err != nil {
-        return false,err
-    }
-	return true,nil
+	// Export the database, and send it to the
+	// bucket in the `db_backups` folder
+	err := mysql.Export().To(config.S3.Path, s3)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
-func (config *Config) exportPostgreDatabase() (bool,error){
+func (config *Config) exportPostgreDatabase() (bool, error) {
 	log.Println("Export Postgre")
-	return true,nil
+	return true, nil
 }
 
-func (config *Config) exportMongodbDatabase() (bool,error){
+func (config *Config) exportMongodbDatabase() (bool, error) {
 	log.Println("Export mongodb")
-	return true,nil
+	return true, nil
 }
 
 func (adapter Adapter) String() string {
- return adapters[adapter - 1]
+	return adapters[adapter-1]
 }
